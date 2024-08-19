36b82d92dcecbf45fd170e60670a37ec6cbaabc7
TarviLion <tarvilionhack@gmail.com>
2024-08-18 23:11:12 -0400

    feat:d

--- .sh/push.sh
+++ .sh/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+echo -e "\e[93mAcuérdate de hacer\e[0m \e[44mgit pull\e[0m"
 read -p "Mensaje del commit: " msg
-
+echo -e "\e[96mIniciando la subida que sube\e[0m"
 git add *
 git commit -m "feat:$msg"
 git push origin main
